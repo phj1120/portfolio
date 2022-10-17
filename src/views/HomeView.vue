1d3d95eaf50c00404b48e9964255bff77436d1d8
phj1120 <hyeonj1998@naver.com>
2022-10-17 18:28:09 +0900

    모각코 그래프 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div>
+      <LineChartGenerator v-if ="loaded" :chart-options="chartOptions" :chart-data="chartData" :chart-id="chartId"
+      :dataset-id-key="datasetIdKey" :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width="width"
+      :height="height" />
+    </div>
+
     <div class="section">
       <div class="header">프로젝트 목록</div>
       <v-layout wrap>
@@ -39,24 +45,95 @@
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-import "swiper/swiper-bundle.css";
-import { Autoplay } from "swiper";
+import { Line as LineChartGenerator } from 'vue-chartjs/legacy'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement
+} from 'chart.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement
+)
+
 export default {
   name: "Home",
   components: {
-    Swiper,
-    SwiperSlide,
+    LineChartGenerator
   },
   data() {
     return {
-      swiperOption: {
-        loop: true,
-        autoplay: {
-          delay: 100,
-        },
+      loaded: true,
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            label: '공부 시간(분)',
+            backgroundColor: '#f87979',
+            data: []
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
       },
     };
+  },
+  async mounted() {
+    this.loaded = false
+    try {
+      await this.axios
+        .get("/api/users/1")
+        .then((result) => {
+          this.chartData.labels= result.data.data.total.dateList
+          this.chartData.datasets[0].data = result.data.data.total.studyTimeList
+        })
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'line-chart'
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => { }
+    },
+    plugins: {
+      type: Array,
+      default: () => []
+    }
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,12 @@ module.exports = defineConfig({
   outputDir: "docs",
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
+  devServer: {
+    proxy: {
+      "/api": {
+        // target: "http://localhost:11201",
+        target: "http://mogakco.parkh.xyz:11201/",
+      },
+    },
+  },
 });
